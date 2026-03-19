e939ae61a0aa49a6d248212171244ef30289cbc7
rbandi23 <rbandi6@gatech.edu>
2026-03-19 11:53:25 -0400

    nextjs change

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -25,6 +25,7 @@ declare module "next-auth/jwt" {
 }
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
